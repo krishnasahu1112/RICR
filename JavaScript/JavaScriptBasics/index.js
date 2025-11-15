12cd2b43d5cab9e0739d618832a7db48fbf081ed
krishnasahu1112 <sahukrishna9584@gmail.com>
2025-11-15 21:47:33 +0530

    Vault of codes task 1 and 2 done

---
+++ JavaScript/JavaScriptBasics/index.js
@@ -0,0 +1,20 @@
+var a = 5;
+var a = 10;
+let b = 19;
+// let b =7; this cant be done becaiuse it is let
+console.log(a,b);
+
+let arr = ["ABC", 1234, 23.45, true, false, 'a'];
+
+console.log(arr);
+
+console.error("This is error");
+console.warn("This is warning");
+alert("Are u confirm to submit, details wont be changed");
+
+if(confirm("Are u sure")){
+    console.log("thanks");
+}
+else{
+    console.error("its ok");
+}
